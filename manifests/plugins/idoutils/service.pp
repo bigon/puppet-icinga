e5343d485129cab0f9aa6f6461cc4e92f4c845ca
Laurent Bigonville <bigon@bigon.be>
2013-09-05 16:17:17 +0200

    Add idoutils_enabled parameter

--- manifests/plugins/idoutils/service.pp
+++ manifests/plugins/idoutils/service.pp
@@ -3,10 +3,21 @@
 # This class provides the idoutils plugin's service.
 #
 class icinga::plugins::idoutils::service {
-  service { $icinga::idoutils_service:
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
-    subscribe => Class['icinga::plugins::idoutils::config'];
+  if ($::icinga::idoutils_enabled == true) {
+    service { $icinga::idoutils_service:
+      ensure    => running,
+      enable    => true,
+      hasstatus => true,
+      subscribe => Class['icinga::plugins::idoutils::config'];
+    }
+    if ($::osfamily == 'Debian'){
+      augeas { "IDO2DB":
+        context => "/files/etc/default/icinga/",
+        changes => [
+          "set IDO2DB yes",
+        ],
+	before  => Service["$icinga::idoutils_service"],
+      }
+    }
   }
 }
